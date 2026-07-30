820ddf2b8ae254e671978c7300b29586b23a9c18
Sunaina <sunaina0798.becse24@chitkara.edu.in>
2026-07-30 14:23:44 +0530

    corrected typos and added functionality using js

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ function addTransaction(e){
     localStorage.setItem("transactions", JSON.stringify(transactions));
 
     updateTransactionList();
-    // updateSummary();
+    updateSummary();
 
     transactionFormEle.reset();
 }
@@ -39,10 +39,9 @@ function addTransaction(e){
 function updateTransactionList(){
     transactionListEle.innerHTML = "";
 
-    // WHAT DOES THIS MEAN [...transactions] ?
+    // WHAT DOES THIS MEAN [...transactions] ? it means we are creating a new array with the same elements as the transactions array. This is done to avoid mutating the original transactions array when we reverse it.
     const sortedTransactions = [...transactions].reverse();
 
-    // will give us the WHAT
     sortedTransactions.forEach((transaction) => {
         const transactionEle = createTransactionElement(transaction);
 
@@ -54,17 +53,55 @@ function updateTransactionList(){
 function createTransactionElement(transaction){
     const listItem = document.createElement("li");
     listItem.classList.add("transaction");
-    // how does the code know to colour it in green or red based on what we are sending here in income and expenses ?
+
     listItem.classList.add(transaction.amount > 0 ? "income" : "expense");
 
-    // TODO:update the amount formating 
+    
     listItem.innerHTML = `
     <span>${transaction.description}</span>
     <span>
-        ${transaction.amount}
+        ${formatCurrency(transaction.amount)}
         <button class = "delete-btn" onclick = "removeTransaction(${transaction.id})">x</button> 
     </span>
     `
 
     return listItem;
 }
+
+function updateSummary(){
+    // WHAT DOES REDUCE DO? IT IS A METHOD THAT TAKES AN ARRAY AND REDUCES IT TO A SINGLE VALUE. IN THIS CASE, WE ARE USING IT TO CALCULATE THE BALANCE BY SUMMING UP ALL THE TRANSACTION AMOUNTS.
+    // 100, -50, 200, -200 => 0+ 100 -50 + 200 - 200 = 50 <-- output 
+    const balance = transactions.reduce((acc, transaction)=> acc+transaction.amount, 0);
+
+    const income = transactions
+    .filter(transaction => transaction.amount >0)
+    .reduce((acc, transaction) => acc+transaction.amount ,0);
+
+    const expense = transactions
+    .filter(transaction => transaction.amount <0)
+    .reduce((acc, transaction) => acc+transaction.amount ,0);
+
+    balanceEle.textContent = formatCurrency(balance);
+    incomeAmountEle.textContent = formatCurrency(income);
+    expenseAmountEle.textContent = formatCurrency(expense);
+}
+
+function formatCurrency(number){
+    return new Intl.NumberFormat("hi-IN", {
+        style:"currency",
+        currency: "INR",
+    }).format(number);
+}
+
+function removeTransaction(id){
+    // filter out the once we want to delete 
+    transactions = transactions.filter(transaction => transaction.id!== id);
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+
+    updateTransactionList();
+    updateSummary();
+}
+// initial render 
+updateTransactionList();
+updateSummary();
+
